8e5ef93bf6499882ca7002bef024ccd7544ca761
TAksenov <taksenov@gmail.com>
2021-01-30 16:30:57 +0500

    javascript-1: feat: draft of compute v.1

---
+++ src/javascript/js-1/compute.md
@@ -0,0 +1,15 @@
+# Курс OTUS JavaScript Developer. Professional
+
+## Студент: Аксенов Тимофей (@taksenov)
+
+Используется `ts-node`
+
+```sh
+npx ts-node ./src/javascript/js-1/compute.ts
+```
+
+Тесты
+
+```sh
+npm run test
+```
--- src/javascript/js-1/compute.ts
+++ src/javascript/js-1/compute.ts
@@ -2,3 +2,24 @@
 export const sum = (a: number, b: number) => {
   return a + b;
 };
+
+/**
+ * Функция расчета максимального списка рекомендаций
+ *
+ * @param {Array} matrix
+ * @returns {Array} result
+ */
+const maxItemAssociation: (m: string[][]) => string[] = matrix => {
+  console.log('INPUT:', matrix);
+
+  return ['1', '2', '3'];
+};
+
+const matrix = [
+  ['Пальто', 'Макароны', 'Пальто', 'Монитор', 'Сервиз'],
+  ['Конструктор', 'Монитор', 'Сервиз', 'Ковер', 'Пальто'],
+  ['Сервиз', 'Монитор', 'Монитор', 'Конструктор', 'Пальто'],
+  ['Сервиз', 'Пальто', 'Макароны', 'Монитор', 'Седло'],
+];
+
+console.log(maxItemAssociation(matrix));
