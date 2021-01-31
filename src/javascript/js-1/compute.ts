aeeaac7988f872211ca2cd7d481b4ee56835c0f3
TAksenov <taksenov@gmail.com>
2021-01-31 17:47:36 +0500

    javascript-1: docs: ./src/javascript/js-1/README.md

--- src/javascript/js-1/compute.ts
+++ src/javascript/js-1/compute.ts
@@ -3,11 +3,3 @@ import rectanglify from './utils/rectanglify';
 import maxItemAssociation from './utils/maxItemAssociation';
 
 export const compute = pipe(rectanglify, maxItemAssociation);
-
-// try {
-//   compute(matrix4);
-// } catch (error) {
-//   if (error) {
-//     console.log('Err 1 occured:', error.message);
-//   }
-// }
